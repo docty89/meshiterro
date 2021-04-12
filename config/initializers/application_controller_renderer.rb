bd382b8aee9f5cac95f5034fd4490a4165a3b958
docty89 <docty89@gmail.com>
2021-04-12 07:08:44 +0000

    meshiterro.2

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd8d27d27e3e87305b3e9406e281380f3043cc81394da7f21338792fc9b449ce4fb2c6cf7e1bd7eae58624d7259895cecc8f4e0cbd20b6e9cedac12f2bce9428f'
